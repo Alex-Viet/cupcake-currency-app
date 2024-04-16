53c2f4fc43c8961e0a05d12d0c7b88ee4334c5ea
Alex-Viet <alex.night.power@gmail.com>
2024-04-16 15:20:20 +0300

    add table and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from '../node_modules/react-router-dom/dist/index'
 import { AppRoutes } from './routes/Routes'
+import './styles.css'
 
 interface AppProps {
   tab: string
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { fetchData } from '../../api/fetchData'
+import { container, table } from './styles/styles.module.css'
 
 export const MainPage: React.FC = () => {
-  const [rates, setRates] = useState<Record<string, number>>({})
+  const [rates, setRates] = useState<Record<string, Record<string, number>>>({})
 
   useEffect(() => {
     const fetchDataAsync = async () => {
       try {
-        const [firstData, secondData, thirdData] = await Promise.all([
+        const [first, second, third] = await Promise.all([
           fetchData('first'),
           fetchData('second'),
           fetchData('third'),
@@ -15,9 +16,9 @@ export const MainPage: React.FC = () => {
 
         setRates((prevRates) => ({
           ...prevRates,
-          firstData,
-          secondData,
-          thirdData,
+          first,
+          second,
+          third,
         }))
       } catch (error) {
         console.error('Error fetching data:', error)
@@ -27,5 +28,28 @@ export const MainPage: React.FC = () => {
     fetchDataAsync()
   }, [])
 
-  return <div></div>
+  return (
+    <div className={container}>
+      <table className={table}>
+        <thead>
+          <tr>
+            <th>Pair name/market</th>
+            <th>First</th>
+            <th>Second</th>
+            <th>Third</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(rates).map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value.RUB.toFixed(3)}</td>
+              <td>{value.USD.toFixed(3)}</td>
+              <td>{value.EUR.toFixed(3)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
